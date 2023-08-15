f410ee9e79a066c9265c9e7d54debb2a2a2f53af
Avneet Kaur <106433236+avneet-kaur9@users.noreply.github.com>
2023-08-14 22:30:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 /*********************************************************************************
- *  WEB322 – Assignment 05
+ *  WEB322 – Assignment 06
  *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
  *  assignment has been copied manually or electronically from any other source (including web sites) or
  *  distributed to other students.
  *
  *  Name: Avneet Kaur 
  * Student ID: 164275216 
- * Date: 23/07/2023
+ * Date: 12/08/2023
  *
  *  Cyclic Web App URL: https://outstanding-blue-flannel-shirt.cyclic.app/about
  *
