6ed314d6b84d87871c6ca4af41e8218d8ffc4995
Avneet Kaur <106433236+avneet-kaur9@users.noreply.github.com>
2023-08-14 22:40:26 -0400

    Update store-service.js

--- store-service.js
+++ store-service.js
@@ -206,4 +206,114 @@ const storeService = {
   deleteItemById,
 };
 
+const generateResponse = (ok, data, status, statusText, request) => ({
+  ok,
+  data,
+  status,
+  statusText,
+  request
+});
+
+function initializeFirestore(keys) {
+  try {
+    firestore = firebase.initializeApp(keys).firestore();
+    userAdmin = firebase.initializeApp(keys, 'userAdmin');
+    firestore.settings({ timestampsInSnapshots: true });
+    return generateResponse(true, firestore, SUCCESS_CODES.OK, STATUS.OK, REQUEST.INITIALIZE);
+  } catch (error) {
+    return generateResponse(false, error, CLIENT_ERROR_CODES.BAD_REQUEST, STATUS.FAILURE, REQUEST.INITIALIZE);
+  }
+}
+
+async function getData({ pathname }, body = {}) {
+  try {
+    const { id, path } = getPathAndElementId(pathname);
+    let data = firestore.collection(path);
+    data = await (id ? queryForID(data, id) : queryForCollection(data, body));
+    return generateResponse(true, data, SUCCESS_CODES.OK, STATUS.OK, REQUEST.GET);
+  } catch (error) {
+    return generateResponse(false, error, CLIENT_ERROR_CODES.BAD_REQUEST, STATUS.FAILURE, REQUEST.GET);
+  }
+}
+
+async function createDoc({ pathname }, body) {
+  try {
+    const { path } = getPathAndElementId(pathname);
+    const data = await firestore
+      .collection(path)
+      .add(body)
+      .then(ref => ref.id);
+    return generateResponse(true, data, SUCCESS_CODES.CREATED, STATUS.OK, REQUEST.POST);
+  } catch (error) {
+    return generateResponse(false, error, CLIENT_ERROR_CODES.BAD_REQUEST, STATUS.FAILURE, REQUEST.POST);
+  }
+}
+
+async function modifyDoc({ pathname }, body) {
+  try {
+    const { id, path } = getPathAndElementId(pathname);
+    const data = await firestore
+      .collection(path)
+      .doc(id)
+      .set(body);
+    return generateResponse(true, data, SUCCESS_CODES.OK, STATUS.OK, REQUEST.PUT);
+  } catch (error) {
+    return generateResponse(false, error, CLIENT_ERROR_CODES.BAD_REQUEST, STATUS.FAILURE, REQUEST.PUT);
+  }
+}
+
+async function deleteDoc({ pathname }) {
+  try {
+    const { id, path } = getPathAndElementId(pathname);
+    const data = await firestore
+      .collection(path)
+      .doc(id)
+      .delete();
+    return generateResponse(true, data, SUCCESS_CODES.OK, STATUS.OK, REQUEST.DELETE);
+  } catch (error) {
+    return generateResponse(false, error, CLIENT_ERROR_CODES.BAD_REQUEST, STATUS.FAILURE, REQUEST.DELETE);
+  }
+}
+
+async function login(email, password) {
+  try {
+    await firebase.auth().signInWithEmailAndPassword(email, password);
+    const data = await firebase.auth().currentUser;
+    return generateResponse(true, data, SUCCESS_CODES.OK, STATUS.OK, REQUEST.LOGIN);
+  } catch (error) {
+    return generateResponse(false, error, CLIENT_ERROR_CODES.UNAUTHORIZED, STATUS.FAILURE, REQUEST.LOGIN);
+  }
+}
+
+async function signUp(email, password) {
+  try {
+    await userAdmin.auth().createUserWithEmailAndPassword(email, password);
+    const data = await userAdmin.auth().currentUser;
+    return generateResponse(true, data, SUCCESS_CODES.OK, STATUS.OK, REQUEST.SIGN_UP);
+  } catch (error) {
+    return generateResponse(false, error, CLIENT_ERROR_CODES.FORBIDDEN, STATUS.FAILURE, REQUEST.SIGN_UP);
+  }
+}
+
+async function updateProfile(body) {
+  try {
+    const user = firebase.auth().currentUser;
+    await user.updateProfile(body);
+    return generateResponse(true, null, SUCCESS_CODES.OK, STATUS.OK, REQUEST.UPDATE_PROFILE);
+  } catch (error) {
+    return generateResponse(false, error, CLIENT_ERROR_CODES.BAD_REQUEST, STATUS.FAILURE, REQUEST.UPDATE_PROFILE);
+  }
+}
+
+const firestoreService = {
+  initialize: keys => initializeFirestore(keys),
+  get: (path, body) => getData(url.parse(path, true), body),
+  put: (path, body) => modifyDoc(url.parse(path, true), body),
+  delete: path => deleteDoc(url.parse(path, true)),
+  post: (path, body) => createDoc(url.parse(path, true), body),
+  patch: (path, body) => modifyDoc(url.parse(path, true), body),
+  login: (email, password) => login(email, password),
+  signUp: (email, password) => signUp(email, password),
+  updateProfile: body => updateProfile(body)
+};
 module.exports = storeService;
